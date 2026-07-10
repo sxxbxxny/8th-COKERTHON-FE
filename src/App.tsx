ab93b6df93cd9f2b45b31e82d5bbb6a2898ec244
sumin <sumin990103@naver.com>
2026-07-11 06:40:52 +0900

    feat: 마이페이지 세 번째 완료 화면 구현

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import MyCh from './pages/my_ch'
 import My from './pages/my'
 import MyComplete1 from './pages/my/complete1'
 import MyComplete2 from './pages/my/complete2'
+import MyComplete3 from './pages/my/complete3'
 import MyStep2 from './pages/my/step2'
 import MyStep3 from './pages/my/step3'
 import Mvp1 from './pages/mvp1'
@@ -45,6 +46,7 @@ function App() {
       <Route path="/my/step1" element={<My />} />
       <Route path="/my/complete1" element={<MyComplete1 />} />
       <Route path="/my/complete2" element={<MyComplete2 />} />
+      <Route path="/my/complete3" element={<MyComplete3 />} />
       <Route path="/my/step2" element={<MyStep2 />} />
       <Route path="/my/step3" element={<MyStep3 />} />
       <Route path="/mission/step1" element={<Step1 />} />
